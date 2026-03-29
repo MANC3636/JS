6542453396b94c4da5b8ed2c36c924134f3de402
Amy Tesfaye <ttyson@blackstudentfund.org>
2026-03-29 03:28:56 -0400

    various demonstrative loops

--- BasicListLoop/listLoopStudy.js
+++ BasicListLoop/listLoopStudy.js
@@ -1,19 +1,40 @@
 
+
         const items = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
-/* 
-        const list = document.getElementById('myList');
-
-        items.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = item;
-            list.appendChild(li);
-        }); */
-
-        const displayItem = document.getElementById('displayItem');//see line 9
+        const berries = ['Strawberry', 'Blueberry', 'Raspberry', 'Blackberry', 'Elderberry'];
         
         let text = '';
+
+
+        
+        const list = document.getElementById('myList');
+
+        const displayItem = document.getElementById('displayItem');//see line 9/* 
+       
+       /*  
         for (let i = 0; i < items.length; i++) {
-            text += items[i] + ' ';
+            //console.log(items[i].length);//1st instruction in the loop, it will print the length of each item in the array
+            if (items[i].length > 5) 
+                {text += items[i] + ' ';}
+            
+        }  */
+       //syntax to see if an item that is in the items array is in the berries array
+        for (let i = 0; i < items.length; i++) {
+            if (berries.includes(items[i])) {
+                text += items[i] + ' ';
+            }
         }
+
+        for (let i = 0; i < items.length; i++) {    //console.log(items[i].length);//1st instruction in the loop, it will print the length of each item in the array
+            //2nd instruction, it will check if the length of the item is greater than 5
+                if (items[i].length < 6) {
+                    //change the color of the text to red if the length of the item is less than 6, but only for the items that are in the berries array
+                    if (berries.includes(items[i])) {   
+                    displayItem.style.color = 'red';
+                   
+                   text += items[i] + ' ';}
+                }
+            }
         // displayItem.textContent = text;
+        
         displayItem.innerHTML=text
