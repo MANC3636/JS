cb2951b9aeee46088a4f385ecd8df64caa53095f
Amy Tesfaye <ttyson@blackstudentfund.org>
2026-03-29 03:42:39 -0400

    played with vars text and text1

--- BasicListLoop/listLoopStudy.js
+++ BasicListLoop/listLoopStudy.js
@@ -3,7 +3,7 @@
         const items = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
         const berries = ['Strawberry', 'Blueberry', 'Raspberry', 'Blackberry', 'Elderberry'];
         
-        let text = '';
+        let text = 'I am in displayItem: ';
 
 
         
@@ -21,20 +21,19 @@
        //syntax to see if an item that is in the items array is in the berries array
         for (let i = 0; i < items.length; i++) {
             if (berries.includes(items[i])) {
-                text += items[i] + ' ';
+                text += items[i] + '; ';
             }
         }
-
-        for (let i = 0; i < items.length; i++) {    //console.log(items[i].length);//1st instruction in the loop, it will print the length of each item in the array
-            //2nd instruction, it will check if the length of the item is greater than 5
-                if (items[i].length < 6) {
-                    //change the color of the text to red if the length of the item is less than 6, but only for the items that are in the berries array
-                    if (berries.includes(items[i])) {   
-                    displayItem.style.color = 'red';
-                   
-                   text += items[i] + ' ';}
-                }
+    let text1 = 'I am in displayItem:'; 
+        for (let i = 0; i < items.length; i++) {   
+            
+                if (items[i].length > 6) {
+                  if (berries.includes(items[i])) 
+                        { 
+                            displayItem.style.color = 'red';                   
+                         text1 += " "+items[i] + '!';}
+                    }
             }
         // displayItem.textContent = text;
         
-        displayItem.innerHTML=text
+        displayItem.innerHTML=text+ text1
